a2e12fb615c06a9eec7f12be9711db320510dac1
VioletYao <violetyaoegg@outlook.com>
2024-01-26 01:25:45 +0800

    change charts calculation, add bar and line charts

--- model.py
+++ model.py
@@ -4,20 +4,38 @@ import re
 import os
 import random
 
+category_specific = {'Dress&Skirts': ['dresses', 'skirts'],
+                     'Jackets&Coats&Outenvear': ['coats', 'jackets'],
+                     'Topweights': ['blouses and woven tops', 'knits and jersey tops', 'shirts', 'tops', 'sweaters'],
+                     'Trousers&Shorts': ['trousers', 'jumpsuits', 'shorts']}
+
+years = ['2019', '2020', '2021', '2022', '2023']
+
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
+fig3, ax3 = plt.subplots()
+
+
 class Result:
     def __init__(self, description):
         self.description = description
 
 
 def get_content(year, season, category, brand):
+    # plt.clf()
     print(str(year) + str(season) + str(category) + str(brand))
     metrics = cal_metrics(year, season, category, brand)
-    chart_path = pie_chart(metrics, year, season, category, brand)
+    sub_metrics_ind = random.randint(0, len(metrics.keys()) - 1)
+    sub_metrics_name = list(metrics.keys())[sub_metrics_ind]
+    sub_metrics = metrics.get(sub_metrics_name)
+    chart_path = pie_chart(year, season, category, brand)
     print(str(metrics))
+    bar_path = bar_char(sub_metrics, year, season, category, brand, sub_metrics_name)
+    line_path = line_char(season, category, brand)
     img_path = search_img(year, season, category, brand)
     # random select k imgs
     img_ind = random.sample(range(len(img_path)), 3)
-    return chart_path, img_path[img_ind[0]], img_path[img_ind[1]], img_path[img_ind[2]]
+    return chart_path, bar_path, line_path, img_path[img_ind[0]], img_path[img_ind[1]], img_path[img_ind[2]]
 
 
 def cal_metrics(year, season, category, brand):
@@ -29,60 +47,126 @@ def cal_metrics(year, season, category, brand):
     :param brand:
     :return:
     '''
-    metrics = dict()
-    with open("data/2019_data/1_cate_tags/" + category + ".json", "r") as file:
-        json_string = file.read()
-    data = json.loads(json_string)
-    attributes = data.keys()
-    for attribute in attributes:
-        sub_metrics = dict()
-        types = data[attribute].keys()
-        sum = 0
-        for item in types:
-            sum += data[attribute][item]
-        for item in types:
-            share = data[attribute][item] / sum
-            sub_metrics[item] = share
-        metrics[attribute] = sub_metrics
+    if season == 'ss':
+        season = "springsummer"
+    metrics = {}
+    data_format = {}
+    for b in brand:
+        with open(
+                'data/all_brand_data_2019_2023/' + b + '-' + season + '-' + year +
+                '-original/category_attribute_count.json', 'r') as file:
+            json_string = file.read()
+        data = json.loads(json_string)
+        amount = 0
+        with open(
+                'data/all_brand_data_2019_2023/' + b + '-' + season + '-' + year +
+                '-original/category_count.json', 'r') as file2:
+            json2 = file2.read()
+        data2 = json.loads(json2)
+        # convert to specific categories according to dictionary above
+        categories = category_specific[category]
+        for item in categories:
+            if item in data and item in data2:
+                data_format = merge_dictionaries(data[item], data_format)
+                amount += data2[item]
+
+        attributes = data_format.keys()
+        for attribute in attributes:
+            sub_metrics = dict()
+            types = data_format[attribute].keys()
+            # sum = 0
+            # for item in types:
+            #     sum += data_format[attribute][item]
+            for item in types:
+                share = data_format[attribute][item] / amount
+                sub_metrics[item] = share
+            metrics[attribute] = sub_metrics
     return metrics
 
 
-def pie_chart(metrics, year, season, category, brand):
+def cal_share(year, season, category, brand):
+    overall_amount = 0
+    amount = 0
+    for b in brand:
+        with open(
+                'data/all_brand_data_2019_2023/' + b + '-' + season + '-' + year +
+                '-original/category_count.json', 'r') as file2:
+            json2 = file2.read()
+        data2 = json.loads(json2)
+        # convert to specific categories according to dictionary above
+        categories = category_specific[category]
+        for key in data2.keys():
+            overall_amount += data2[key]
+        for item in categories:
+            amount += data2[item] if item in data2 else 0
+    ratio = amount / overall_amount
+    return ratio
+
+
+def pie_chart(year, season, category, brand):
     '''
-    根据metrics里的share画饼图
-    :param metrics:
     :param year:
     :param season:
     :param category:
     :param brand:
     :return:
     '''
-    title = list(metrics.keys())[0]
-    metrics = metrics[list(metrics.keys())[0]]
-    plt.pie(metrics.values(), labels=metrics.keys(), autopct='%.2f%%')
-
+    ax1.clear()
+    ratio = cal_share(year, season, category, brand)
+    ax1.pie([ratio, 1 - ratio], labels=[category, 'other category'], autopct='%.2f%%')
     chart_path = "data/charts/pie/" + category + ".png"
-    plt.title(title)
-    plt.savefig(chart_path, transparent=True)
+    ax1.set_title("Share of "+ category)
+    fig1.savefig(chart_path, transparent=True)
+    return chart_path
+
+
+def bar_char(metrics, year, season, category, brand, sub_metrics_name):
+    ax2.clear()
+    if int(year) > 2019:
+        metrics_previous = cal_metrics(str(int(year) - 1), season, category, brand)
+        sub_metrics_previous = metrics_previous.get(sub_metrics_name)
+        minus_metrics = minus_dict(metrics, sub_metrics_previous)
+        ax2.barh(list(minus_metrics.keys()), list(minus_metrics.values()), align='edge')
+        for i, v in enumerate(list(minus_metrics.values())):
+            ax2.text(v + 1, i, str(v), va='center', fontsize=3)
+        bar_path = "data/charts/bar/" + category + ".png"
+        ax2.set_title(sub_metrics_name + ' compared with last year')
+        fig2.savefig(bar_path, transparent=True)
+        return bar_path
+    else:
+        return None
+
+
+def line_char(season, category, brand):
+    ax3.clear()
+    share = list()
+    for year in years:
+        share.append(cal_share(year, season, category, brand))
+    ax3.plot(years, share)
+    chart_path = "data/charts/line/" + category + ".png"
+    ax3.set_title("Change of " + category + " share in 2019-2023")
+    fig3.savefig(chart_path, transparent=True)
     return chart_path
 
 
 def search_img(year, season, category, brands):
+    categories = category_specific[category]
     directory_path = "data/refined_tag_files/"
     img_path = list()
     file_names = os.listdir(directory_path)
     if season == 'ss':
         season = "springsummer"
-    for brand in brands:
-        brand_pattern = re.compile(brand, re.IGNORECASE)
-        year_pattern = re.compile(year)
-        season_pattern = re.compile(season, re.IGNORECASE)
-        for file in file_names:
-            if brand_pattern.search(file) and year_pattern.search(file) and season_pattern.search(file):
-                img_name = find_items_with_category(file, category, directory_path)
-                for item in img_name:
-                    item = item[5:]
-                    img_path.append('data/2019-2023ss/' + brand + '-' + season + '-' + year + '-original/' + item)
+    for cata in categories:
+        for brand in brands:
+            brand_pattern = re.compile(brand, re.IGNORECASE)
+            year_pattern = re.compile(year)
+            season_pattern = re.compile(season, re.IGNORECASE)
+            for file in file_names:
+                if brand_pattern.search(file) and year_pattern.search(file) and season_pattern.search(file):
+                    img_name = find_items_with_category(file, cata, directory_path)
+                    for item in img_name:
+                        item = item[5:]
+                        img_path.append('data/2019-2023ss/' + brand + '-' + season + '-' + year + '-original/' + item)
     return img_path
 
 
@@ -105,3 +189,32 @@ def find_items_with_category(filename, category, directory_path):
             items_with_category.append(item)
 
     return items_with_category
+
+
+def merge_dictionaries(dict1, dict2):
+    merged_dict = {}
+    # Combine keys from both dictionaries
+    all_keys = set(dict1.keys()).union(set(dict2.keys()))
+
+    for key1 in all_keys:
+        merged_dict[key1] = {}
+        # Combine keys from the second layer
+        dict1_keys = dict1.get(key1, {})
+        dict2_keys = dict2.get(key1, {})
+        all_keys_layer2 = set(dict1_keys.keys()).union(set(dict2_keys.keys()))
+
+        for key2 in all_keys_layer2:
+            # Combine keys from the third layer and sum values if keys match
+            value1 = dict1_keys.get(key2, 0)
+            value2 = dict2_keys.get(key2, 0)
+            merged_dict[key1][key2] = value1 + value2
+
+    return merged_dict
+
+
+def minus_dict(dict1, dict2):
+    minus_dict = {}
+    all_keys = set(dict1.keys()).union(set(dict2.keys()))
+    for key in all_keys:
+        minus_dict[key] = dict1.get(key, 0) - dict2.get(key, 0)
+    return minus_dict
--- page.py
+++ page.py
@@ -31,9 +31,9 @@ css = """
 .related_img {
     height: 30vh;
     width: auto;
-    object-fit: contain; /* Add this line */
-    display: block; /* This ensures that the image doesn't have extra space below it */
-    margin: auto;
+    # object-fit: contain; /* Add this line */
+    # display: block; /* This ensures that the image doesn't have extra space below it */
+    # margin: auto;
 }
 
 """
@@ -41,12 +41,11 @@ css = """
 with gr.Blocks(css=css) as demo:
     with gr.Row():
         with gr.Column(scale=1):
-            year = gr.Dropdown(['2019', '2020', '2021', '2022', '2023', '2024'], label='year', value='2019')
-            season = gr.Dropdown(['ss', 'winter'], label='season', value='ss')
+            year = gr.Dropdown(['2019', '2020', '2021', '2022', '2023'], label='year', value='2019')
+            season = gr.Dropdown(['springsummer', 'winter'], label='season', value='springsummer')
             category = gr.Dropdown(
-                ['blouses and woven tops', 'coats', 'dresses', 'jackets', 'jumpsuits', 'knits and jersey tops',
-                 'shirts', 'shorts', 'skirts', 'sweaters', 'tops', 'trousers'],
-                value='coats', label='category')
+                ['Dress&Skirts', 'Jackets&Coats&Outenvear', 'Topweights', 'Trousers&Shorts'],
+                value='Dress&Skirts', label='category')
             brand = gr.CheckboxGroup(
                 ['chanel', 'christian-dior', 'givenchy', 'louis-vuitton', 'saint-laurent', 'valentino'],
                 value=['chanel'], label='brand')
@@ -60,22 +59,23 @@ with gr.Blocks(css=css) as demo:
                     with gr.Column(scale=2):
                         gr.Image(value='content/img.png', min_width=500)
     with gr.Row():
-        with gr.Column(variant='panel', elem_classes=['page2']):
+        with gr.Column(elem_classes=['page2']):
             with gr.Row():
-                with gr.Column(variant='panel'):
+                with gr.Column(variant='panel', scale=2):
                     gr.Markdown("""
                     # Description
                     """)
-                with gr.Column(variant='panel'):
+                with gr.Column(variant='panel', scale=2):
                     gr.Markdown("# Charts")
                     chart_path = gr.Image()
-                    gr.Image(value="content/img_1.png")
-                    gr.Image(value="content/img_2.png")
-                with gr.Column(variant='panel'):
-                    img1 = gr.Image(elem_classes='related_img')
-                    img2 = gr.Image(elem_classes='related_img')
-                    img3 = gr.Image(elem_classes='related_img')
+                    if year != '2019':
+                        bar_path = gr.Image()
+                    line_path = gr.Image()
+                with gr.Column(variant='panel', scale=1):
+                    img1 = gr.Image()
+                    img2 = gr.Image()
+                    img3 = gr.Image()
     generate.click(fn=get_content, inputs=[year, season, category, brand],
-                   outputs=[chart_path, img1, img2, img3])
+                   outputs=[chart_path, bar_path, line_path, img1, img2, img3])
 
-demo.launch(share=True)
+demo.launch(share=False)
